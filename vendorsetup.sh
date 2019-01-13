f9f603561ba20db69fc32e5f3b4f9d1f414ea409
diowil <diowil4@gmail.com>
2019-01-13 03:18:03 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo cm_hwgra-eng
-add_lunch_combo cm_hwgra-userdebug
-add_lunch_combo cm_hwgra-user
+add_lunch_combo cm_liszt-eng
+add_lunch_combo cm_liszt-userdebug
+add_lunch_combo cm_liszt-user
 
